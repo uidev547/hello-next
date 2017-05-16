624057f709e08151a438253c07da86b40be67cec
Kiran <akiran547@gmail.com>
2017-05-16 20:49:21 +0530

    added links

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
     <p>Hello Next.js</p>
+    <Link href="/about">
+         <button>Go to About Page</button>
+    </Link>
   </div>
 )
 
